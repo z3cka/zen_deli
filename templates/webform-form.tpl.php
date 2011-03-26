dc8ee0a571f6cad70b6567c0a0cf21d74c845ada
Casey Grzecka <c@sey.cm>
2011-03-26 01:05:06 +0000

    made node 1's form look sweeeeeeeet

--- templates/webform-form.tpl.php
+++ templates/webform-form.tpl.php
@@ -26,8 +26,9 @@
 
   // Print out the main part of the form.
   // Feel free to break this up and move the pieces within the array.
-  print drupal_render($form['submitted']);
-
+  echo '<div class="form-seperator">';
+    print drupal_render($form['submitted']);
+  echo '</div>';
   // Always print out the entire $form. This renders the remaining pieces of the
   // form that haven't yet been rendered above.
     echo '<div class="webform-button-wrapper">';
