bed7d606bfe024815d5efdf581d6075dc1956a3b
Casey Grzecka <c@sey.cm>
2011-03-27 21:50:28 +0000

    Made the form look sexier and fixed the numbering to go vertical. TODO: breakout the Group fields out of the grid

---
+++ js/script.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function(){
+  //jQuery("p").mouseover(function(){
+  //jQuery(this).css("color", "yellow");
+  //});
+  jQuery('.form-seperator').columnize({width:320});
+});
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -74,7 +74,7 @@
   <div id="header"><div class="section clearfix">
 
     <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php //print $logo; ?>" alt="<?php print t(''); ?>" /></a>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><!--<img src="<?php //print $logo; ?>" alt="<?php print t(''); ?>" />--></a>
     <?php endif; ?>
 
     <?php if ($site_name || $site_slogan): ?>
