8c7b854eab90de6b23221a81fbd63e0967f7b161
Casey Grzecka <github@z3cka.oib.com>
2011-04-12 23:37:06 -0700

    update theme to use jQuery as pager instead of multi step form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 (function ($) {
-$(document).ready(function(){
-  if ($('.webform-button-wrapper input:nth-child(2)').attr('value') >= '2' ) {
-    $('.form-seperator').columnize({width:330});
-  //$('.page-node-1 .form-item:first-child').addClass('pad-column-tops');
-  };
-});
+  $(document).ready(function(){
+    /*if ($('.webform-button-wrapper input:nth-child(2)').attr('value') >= '2' ) {
+      $('.form-seperator').columnize({width:330});
+    //$('.page-node-1 .form-item:first-child').addClass('pad-column-tops');
+    };*/
+  
+    /*$('#edit-set-2-fieldset-draggable-wrapper').hide();
+    $('#edit-set-3-fieldset-draggable-wrapper').hide();
+  
+    $('#edit-set-1-fieldset-draggable-wrapper').append('<button type="button" class="fancy-next-button">Next Page</button>');
+    $('.fancy-next-button').click(function(){
+      $('#edit-set-1-fieldset-draggable-wrapper').fadeOut('slow');
+      $('#edit-set-2-fieldset-draggable-wrapper').fadeIn('slow');
+    });  */
+  });
 })(jQuery);
